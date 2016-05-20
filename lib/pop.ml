b1e82280c6744e3a9f4292ba2727b7b7ccbb5d28
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-05-20 04:06:30 -0700

    Reorganize Pop module

--- lib/pop.ml
+++ lib/pop.ml
@@ -1,161 +1,167 @@
-open BaseDecoder
-open BaseEncoder
+module D =
+struct
+  open BaseDecoder
+  let p_crlf p state =
+    p_chr '\r' state
+    ; p_chr '\n' state
+    ; p state
 
-let p_crlf p state =
-  p_chr '\r' state
-  ; p_chr '\n' state
-  ; p state
+  let is_atom = function
+    | '\x01' .. '\x07' -> true
+    | chr -> false
 
-let is_atom = function
-  | '\x01' .. '\x07' -> true
-  | chr -> false
+  let p_atom state = p_while is_atom state
+  let p_space = p_chr ' '
 
-let p_atom state = p_while is_atom state
-let p_space = p_chr ' '
+  type command =
+    [ `Quit
+    | `Stat
+    | `List of string option
+    | `Retr of string
+    | `Dele of string
+    | `Noop
+    | `Rset ]
 
-type command =
-  [ `Quit
-  | `Stat
-  | `List of string option
-  | `Retr of string
-  | `Dele of string
-  | `Noop
-  | `Rset ]
+  let p_command p state =
+    let command = p_atom state in
 
-let p_command p state =
-  let command = p_atom state in
+    match String.uppercase command with
+    | "QUIT" -> p_crlf (p `Quit) state
+    | "STAT" -> p_crlf (p `Stat) state
+    | "LIST" ->
+        ((fun state ->
+          p_space state
+          ; let msg = p_atom state in
+            p_crlf (fun state -> `Ok (msg, state)) state)
+         / (p_crlf @ p (`List None))
+         @ fun msg -> p (`List (Some msg)))
+        state
+    | "RETR" ->
+      p_space state
+      ; let msg = p_atom state in
+        p_crlf (p (`Retr msg)) state
+    | "DELE" ->
+      p_space state
+      ; let msg = p_atom state in
+        p_crlf (p (`Dele msg)) state
+    | "NOOP" -> p_crlf (p `Noop) state
+    | "RSET" -> p_crlf (p `Rset) state
+    | c      -> p_crlf (p (`Unknow c)) state
 
-  match String.uppercase command with
-  | "QUIT" -> p_crlf (p `Quit) state
-  | "STAT" -> p_crlf (p `Stat) state
-  | "LIST" ->
-      ((fun state ->
-        p_space state
-        ; let msg = p_atom state in
-          p_crlf (fun state -> `Ok (msg, state)) state)
-       / (p_crlf @ p (`List None))
-       @ fun msg -> p (`List (Some msg)))
-      state
-  | "RETR" ->
-    p_space state
-    ; let msg = p_atom state in
-      p_crlf (p (`Retr msg)) state
-  | "DELE" ->
-    p_space state
-    ; let msg = p_atom state in
-      p_crlf (p (`Dele msg)) state
-  | "NOOP" -> p_crlf (p `Noop) state
-  | "RSET" -> p_crlf (p `Rset) state
-  | c      -> p_crlf (p (`Unknow c)) state
-
-let p_to_crlf p state =
-  let rec aux has_cr state =
-    match cur_chr state with
-    | '\n' when has_cr ->
-      junk_chr state; p state
-    | '\r' ->
-      junk_chr state; aux true state
-    | chr ->
-      junk_chr state; aux false state
-  in
-
-  aux false state
-
-let p_response p state =
-  match cur_chr state with
-  | '+' -> junk_chr state; p_str "OK" state; p_to_crlf (p `Ok) state
-  | '-' -> junk_chr state; p_str "ERR" state; p_to_crlf (p `Err) state
-  | chr -> raise (Error.Error (Error.err_unexpected chr state))
-
-let p_dot p state = p_chr '.' state; p state
-
-let p_body p state =
-  let stop =
-    (p_crlf @ p_dot @ p_crlf (fun state -> `Ok ((), state)))
-    / (fun state -> `Continue state)
-    @ (fun () state -> `Stop state)
-  in
-
-  let buf = Buffer.create 16 in
-
-  let rec aux state =
-    let rec catch = function
-      | `Stop state ->
-        Printf.printf "END\n%!";
-        p (Buffer.contents buf) state
-      | #Error.err as err -> err
-      | `Read (buf, off, len, k) ->
-        Printf.printf "READ\n%!";
-        `Read (buf, off, len, (fun i -> catch @@ safe k i))
-      | `Continue state ->
-        match peek_chr state with
-        | Some chr ->
-          Buffer.add_char buf (cur_chr state)
-          ; junk_chr state
-          ; aux state
-        | None -> read_line aux state
+  let p_to_crlf p state =
+    let rec aux has_cr state =
+      match cur_chr state with
+      | '\n' when has_cr ->
+        junk_chr state; p state
+      | '\r' ->
+        junk_chr state; aux true state
+      | chr ->
+        junk_chr state; aux false state
     in
 
-    catch (stop state)
-  in
+    aux false state
 
-  aux state
+  let p_response p state =
+    match cur_chr state with
+    | '+' -> junk_chr state; p_str "OK" state; p_to_crlf (p `Ok) state
+    | '-' -> junk_chr state; p_str "ERR" state; p_to_crlf (p `Err) state
+    | chr -> raise (Error.Error (Error.err_unexpected chr state))
 
-let is_digit = function
-  | '0' .. '9' -> true
-  | _          -> false
+  let p_dot p state = p_chr '.' state; p state
 
-let p_list p =
-  let rec loop acc =
-    (p_dot @ p_crlf @ fun state -> `Ok ((), state))
-    / (fun state ->
-       let msg = int_of_string @@ p_while is_digit state in
-       p_space state
-       ; let size = int_of_string @@ p_while is_digit state in
-         p_crlf (read_line @ loop ((msg, size) :: acc)) state)
-    @ fun () -> p (List.rev acc)
-  in
+  let p_body p state =
+    let stop =
+      (p_crlf @ p_dot @ p_crlf (fun state -> `Ok ((), state)))
+      / (fun state -> `Continue state)
+      @ (fun () state -> `Stop state)
+    in
 
-  read_line (loop [])
+    let buf = Buffer.create 16 in
 
-let p_noop p state =
-  p state
+    let rec aux state =
+      let rec catch = function
+        | `Stop state ->
+          Printf.printf "END\n%!";
+          p (Buffer.contents buf) state
+        | #Error.err as err -> err
+        | `Read (buf, off, len, k) ->
+          Printf.printf "READ\n%!";
+          `Read (buf, off, len, (fun i -> catch @@ safe k i))
+        | `Continue state ->
+          match peek_chr state with
+          | Some chr ->
+            Buffer.add_char buf (cur_chr state)
+            ; junk_chr state
+            ; aux state
+          | None -> read_line aux state
+      in
 
-let p_err _ = `Err
-let p_ok _  = `Ok
+      catch (stop state)
+    in
 
-let extract_data p = function
-  | `List None -> p_list @ fun l -> p (`List l)
-  | `Retr _    -> p_body @ fun o -> p (`Body o)
-  | _          -> p_ok
+    aux state
 
-let decode state x =
-  let p_data = extract_data (fun x _ -> x) x in
-  (read_line
-   @ p_response
-   @ function
-     | `Err -> p_err
-     | `Ok -> p_data)
-  state
+  let is_digit = function
+    | '0' .. '9' -> true
+    | _          -> false
 
-let w_crlf k state =
-  w "\r\n" k state
+  let p_list p =
+    let rec loop acc =
+      (p_dot @ p_crlf @ fun state -> `Ok ((), state))
+      / (fun state ->
+         let msg = int_of_string @@ p_while is_digit state in
+         p_space state
+         ; let size = int_of_string @@ p_while is_digit state in
+           p_crlf (read_line @ loop ((msg, size) :: acc)) state)
+      @ fun () -> p (List.rev acc)
+    in
 
-let w_command = function
-  | `Quit            -> w "QUIT" $ w_crlf
-  | `Stat            -> w "STAT" $ w_crlf
-  | `List (Some msg) -> w "LIST" & w (string_of_int msg) $ w_crlf
-  | `List None       -> w "LIST" $ w_crlf
-  | `Retr msg        -> w "RETR" & w (string_of_int msg) $ w_crlf
-  | `Dele msg        -> w "DELE" & w (string_of_int msg) $ w_crlf
-  | `Noop            -> w "NOOP" $ w_crlf
-  | `Rset            -> w "RSET" $ w_crlf
-  | `User user       -> w "USER" & w user $ w_crlf
-  | `Pass pass       -> w "PASS" & w pass $ w_crlf
+    read_line (loop [])
 
-let w_ret _ = `Ok
+  let p_noop p state =
+    p state
 
-let encode state cmd = w_command cmd (flush w_ret) state
+  let p_err _ = `Err
+  let p_ok _  = `Ok
+
+  let extract_data p = function
+    | `List None -> p_list @ fun l -> p (`List l)
+    | `Retr _    -> p_body @ fun o -> p (`Body o)
+    | _          -> p_ok
+
+  let decode state x =
+    let p_data = extract_data (fun x _ -> x) x in
+    (read_line
+     @ p_response
+     @ function
+       | `Err -> p_err
+       | `Ok -> p_data)
+    state
+end
+
+module E =
+struct
+  open BaseEncoder
+
+  let w_crlf k state =
+    w "\r\n" k state
+
+  let w_command = function
+    | `Quit            -> w "QUIT" $ w_crlf
+    | `Stat            -> w "STAT" $ w_crlf
+    | `List (Some msg) -> w "LIST" & w (string_of_int msg) $ w_crlf
+    | `List None       -> w "LIST" $ w_crlf
+    | `Retr msg        -> w "RETR" & w (string_of_int msg) $ w_crlf
+    | `Dele msg        -> w "DELE" & w (string_of_int msg) $ w_crlf
+    | `Noop            -> w "NOOP" $ w_crlf
+    | `Rset            -> w "RSET" $ w_crlf
+    | `User user       -> w "USER" & w user $ w_crlf
+    | `Pass pass       -> w "PASS" & w pass $ w_crlf
+
+  let w_ret _ = `Ok
+
+  let encode state cmd = w_command cmd (flush w_ret) state
+end
 
 type t =
   { dec : Decoder.t
@@ -169,7 +175,7 @@ let encode x k c =
     | `Ok -> k c
   in
 
-  loop (encode c.enc x)
+  loop (E.encode c.enc x)
 
 exception Invalid_command
 
@@ -187,7 +193,7 @@ let decode x k c =
       raise (Error.Error err)
   in
 
-  loop (decode c.dec x)
+  loop (D.decode c.dec x)
 
 let connection () =
   let state =
